2d227f2af68d95281375ec233acdb277e43f4633
EmmKall <ing.emmanuel.cal@gmail.com>
2024-06-10 13:27:30 -0600

    Validar fechas en registro

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Models\Book;
+use App\Models\Cuarto;
 use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
 
 class BookController extends Controller
 {
@@ -40,6 +42,20 @@ class BookController extends Controller
             'out'        => 'required',
             'check_in'   => 'required'
         ]);
+        $books = Book::where( 'cuarto_id', '=', $request->cuarto_id )->get();
+        $in = Carbon::parse( $request->in );
+        $out = Carbon::parse( $request->out );
+        //Valid dates
+        foreach ( $books as $key => $item ) {
+            $rowIn = Carbon::parse( $item->in );
+            $rowOut = Carbon::parse( $item->out );
+            if( ( $in == $rowIn || ( $in > $rowIn && $in < $rowOut ) ) || ( $out == $rowOut || ( $out > $rowIn && $out < $rowOut ) ) ) {
+                return [
+                    'status' => 400,
+                    'msg'    => 'Room are not available for these date'
+                ];
+            }
+        }
         $row = Book::create( $request->all() );
         return [
             'status' => 201,
